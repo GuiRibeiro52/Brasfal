cb13dda93ebd2dea6f4d5ffd270eb7480c967dc9
Guilherme Ribeiro <guilherme_ribeiro52@icloud.com>
2024-11-19 16:34:40 -0300

    fix - link para site do desenvolvedor

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
       <footer className="flex bg-footercolor items-center justify-center gap-5 p-10">
         <div className="container mx-auto text-center">
-          <p className="text-white opacity-70 mb-4 md:px-36 lg:px-80">©2024 - BrasFal | Todos os direitos reservados | Desenvolvido por <Link to="https://guiribeiro52.github.io/GuilhermeRibeiro/">Guilherme Ribeiro.</Link></p>
+          <p className="text-white opacity-70 mb-4 md:px-36 lg:px-80">©2024 - BrasFal | Todos os direitos reservados | Desenvolvido por <Link to="https://guilhermeribeiro.dev.br">Guilherme Ribeiro.</Link></p>
         </div>
       </footer>
     );
